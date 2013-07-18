f00b010e7556686c2df16eb17a3fdeb69d5379b1
Lance Ennen <byennen@gmail.com>
2013-07-18 11:41:31 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CahootsConnect::Application.config.secret_token = '19ba2a955cd229ef1ac0af13e23747623186455ed6698a8a86d45b93bae8226f636af5a1b65fd0b41ab8e832e06373aaae6a4a751391f61c4e033836a0cc9830'
