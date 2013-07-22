5a3d41a95c116a810b35df97c3230cbd896ac8f4
Lance Ennen <byennen@gmail.com>
2013-07-22 10:58:40 -0500

    fix mass-assignment issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :role_ids, :as => :admin
-  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :university, :graduation_year, :major
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :university, :graduation_year, :major,
+                  :skills, :education, :experience
 
 end
