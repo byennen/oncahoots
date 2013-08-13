e6d2bea98e380d9487f2873002060384f565b115
Lance Ennen <byennen@gmail.com>
2013-08-13 09:32:05 -0500

    adding profile image to users

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,9 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   has_many :experiences
 
-  attr_accessible :user_id, :education, :experience, :skills, :experiences_attributes
+  attr_accessible :user_id, :education, :experience, :skills, :experiences_attributes, :image
 
   accepts_nested_attributes_for :experiences, allow_destroy: true
+
+  mount_uploader :image, ImageUploader
 end
