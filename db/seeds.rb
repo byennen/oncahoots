e9a6812ea067d0082813c3cec8a00d3964fd3e87
Lance Ennen <byennen@gmail.com>
2013-08-28 16:17:07 -0500

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
-#roles
-puts 'Adding Roles'
-Role.create(name: 'super_admin')
-Role.create(name: 'university_admin')
-Role.create(name: 'member')
-Role.create(name: 'club_owner')
-Role.create(name: 'club_admin')
-Role.create(name: 'event_admin')
+#Roles
+puts 'ROLES'
+YAML.load(ENV['ROLES']).each do |role|
+  Role.find_or_create_by_name({ :name => role }, :without_protection => true)
+  puts 'role: ' << role
+end
 
 #Universities
 puts 'Adding Universities'
@@ -27,11 +25,11 @@ Location.create(name: 'Los Angeles, CA')
 puts 'DEFAULT USERS'
 puts 'Add Super Admin'
 @super_admin = User.find_or_create_by_email(
-  first_name: 'Lance',
-  last_name: 'Ennen',
-  email: 'super_admin@cahoots-connect.com',
-  password: 'cahoot2013',
-  password_confirmation: 'cahoot2013',
+  first_name: ENV['SUPER_ADMIN_FIRST_NAME'].dup,
+  last_name: ENV['SUPER_ADMIN_LAST_NAME'].dup,
+  email: ENV['SUPER_ADMIN_EMAIL'].dup,
+  password: ENV['SUPER_ADMIN_PASSWORD'].dup,
+  password_confirmation: ENV['SUPER_ADMIN_PASSWORD'].dup,
   university_id: @university.id,
   graduation_year: '2005',
   major: 'Computer Science',
@@ -39,15 +37,14 @@ puts 'Add Super Admin'
 )
 @super_admin.add_role :super_admin
 Profile.create(user_id: @super_admin.id, skills: 'Super Admin', education: 'Super Admin', experience: 'Super Admin')
-puts @super_admin.inspect
 
 #university admin user
 @university_admin = User.find_or_create_by_email(
-  first_name: "Carol",
-  last_name: "Quillen",
-  email: 'university_admin@cahoots-connect.com',
-  password: 'cahoot2013',
-  password_confirmation: 'cahoot2013',
+  first_name: ENV['UNIVERSITY_ADMIN_FIRST_NAME'].dup,
+  last_name: ENV['UNIVERSITY_ADMIN_LAST_NAME'].dup,
+  email: ENV['UNIVERSITY_ADMIN_EMAIL'].dup,
+  password: ENV['UNIVERSITY_ADMIN_PASSWORD'].dup,
+  password_confirmation: ENV['UNIVERSITY_ADMIN_PASSWORD'].dup,
   university_id: @university.id,
   graduation_year: '2005',
   major: 'Computer Science',
@@ -55,4 +52,3 @@ puts @super_admin.inspect
 )
 @university_admin.add_role :university_admin
 Profile.create(user_id: @university_admin.id, skills: 'University Admin', education: 'University Admin', experience: 'University Admin')
-puts @university_admin.inspect
