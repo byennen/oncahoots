6aa2aa06699bbd9ddbb79fbaf31e8b53baa7c262
Lance Ennen <byennen@gmail.com>
2013-08-29 10:57:52 -0500

    redirect if not super admin or university admin to home page if trying to access the admin dashboard :+1:

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
+    unless current_user.super_admin? || current_user.university_admin?
+      redirect_to root_path
+    end
   end
 end
