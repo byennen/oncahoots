ffe063f565e460451279873884ceca15d7b991e3
Lance Ennen <byennen@gmail.com>
2013-08-29 14:16:25 -0500

    moving staging http auth to staging env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,20 +5,6 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
-  #password for staging
-  if (ENV["RAILS_ENV"] == "staging")
-    before_filter :verifies_staging_user
-  end
-
-  def verifies_staging_user
-    unless cookies[:cahoots_connect]
-      authenticate_or_request_with_http_basic do |username, password|
-        username == "cahoots" && password == "cahoots2013"
-        cookies[:cahoots_connect] = {:value => "staging", :expires => 5.days.from_now.utc, :domain => :all}
-      end
-    end
-  end
-
   # Idea for profiles -Lance
   #before_filter :checks_for_users_profile
   #def checks_for_users_profile
