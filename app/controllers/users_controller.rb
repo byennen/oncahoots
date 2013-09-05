3ac3eb98d148dfaa5b30fe8c4f77f2a973736356
Jim Ray <jimiray@gmail.com>
2013-09-04 19:23:53 -0600

    show portfolio items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
     @profile = @user.profile
     @experiences = @profile.experiences
     @invitations = Invitation.where(recipient_id: current_user.id)
+    @portfolio_items = @profile.portfolio_items
   end
 end
