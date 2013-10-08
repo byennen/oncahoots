a4aeb897639acaff4c61be04571bf8402c68c646
Jim Ray <jimiray@gmail.com>
2013-10-07 20:54:46 -0600

    merge users and professional page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,9 @@ class UsersController < ApplicationController
     @experiences = @profile.experiences
     @invitations = Invitation.where(recipient_id: current_user.id)
     @portfolio_items = @profile.portfolio_items
+    @messages = current_user.mailbox.conversations
+    @unread_messages = current_user.mailbox.inbox(unread: true)
+    @requests = current_user.relationships.where(status: 'pending')
+    @contacts = current_user.relationships.where(status: 'accepted')    
   end
 end
