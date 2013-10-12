0e94c4e198932a753918227d0585dcf1925501d1
Jeff Powell <powellj@mac.com>
2013-10-11 21:59:29 -0600

    Setup university_event model to be polymorphic

--- app/models/university_event.rb
+++ app/models/university_event.rb
@@ -1,3 +1,7 @@
 class UniversityEvent < ActiveRecord::Base
-  attr_accessible :category, :club_id, :date, :description, :image, :location, :time, :title
+  attr_accessible :title, :date, :time, :location, :description, :category,
+                  :image
+
+  belongs_to :club
+  has_many :events, as: :eventable
 end
