fa0222434e5a2a089948b5404341b2a8456e92ea
Jeff Powell <powellj@mac.com>
2013-10-18 19:24:32 -0600

    Misc attr_accessible cleanup in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,11 +17,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :role_ids
   attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me,
                   :university_id, :location_id, :graduation_year, :major, :double_major, :slug,
-                  :city, :state, :alumni, :professional_field_id
+                  :city, :state, :alumni, :professional_field_id, :role_ids
 
   validates_presence_of :university_id, :graduation_year, :major, :city, :state
 
