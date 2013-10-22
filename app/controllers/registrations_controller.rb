6bd4272aab307cbb8bbcc45cf490dd9702e0bcb3
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-10-23 00:55:44 +0700

    redirect_to edit profile page after sign up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,6 +9,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    new_user_profile_path(current_user)
+    edit_user_profile_path(current_user, current_user.profile)
   end
 end
