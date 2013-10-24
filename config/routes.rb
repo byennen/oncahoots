904204aa0d5c192c3168f421738e427f5ac22e26
Jim Ray <jimiray@gmail.com>
2013-10-23 19:44:14 -0600

    remove index from universities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 CahootsConnect::Application.routes.draw   do
   resources :cities
-  resources :universities, only: [:index, :show] do
+  resources :universities, only: [:show] do
     resources :events
     resources :university_events, :path => 'calendar', :controller => :university_events
     resources :updates, only: [:new, :create, :update, :destroy] do
