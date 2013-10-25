3f7802b1c1163ce049fd7425e9640059360bbb6c
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-10-25 14:55:54 +0700

    register new club on university page - register new tab

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 CahootsConnect::Application.routes.draw   do
   resources :cities
   resources :universities, only: [:show] do
+    collection do
+      post :create_club
+    end
     resources :events
     resources :university_events, :path => 'calendar', :controller => :university_events do
 
