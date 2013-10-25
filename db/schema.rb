a757fce011bcbe1c68ca53c1fe940fcf3b73d96a
Prasad <bhanurorkitchen2@gmail.com>
2013-10-25 12:00:32 +0700

    rake task: add_image_for_all_universities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131024164735) do
+ActiveRecord::Schema.define(:version => 20131025035233) do
 
   create_table "cities", :force => true do |t|
     t.string   "name"
@@ -243,6 +243,7 @@ ActiveRecord::Schema.define(:version => 20131024164735) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "image"
+    t.string   "banner"
   end
 
   create_table "updates", :force => true do |t|
