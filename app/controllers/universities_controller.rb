327f7c5b5c6be88335bfd0c6049675e4b1cec50a
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-10-25 14:55:54 +0700

    register new club on university page - register new tab

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -1,5 +1,18 @@
 class UniversitiesController < ApplicationController
 
+  def create_club
+    @university = current_user.university
+    @club = @university.clubs.new(params[:club])
+    @club.user_id = current_user.id
+    if @club.save
+      @club.memberships.create(user_id: current_user.id, admin: true)
+      redirect_to university_club_path(@university, @club)
+    else
+      load_details_data
+      render :show
+    end
+  end
+
   def index
     @universities = University.all
   end
@@ -24,6 +37,7 @@ class UniversitiesController < ApplicationController
       @university_events = @university.events.all
       @update = Update.new
       @clubs = @university.clubs.order(:name)
+      @club ||= @university.clubs.build
       @club_updates = Update.where(updateable_type: "Club").where(updateable_id: @clubs.map(&:id)).order("created_at DESC").all
     end
 end
