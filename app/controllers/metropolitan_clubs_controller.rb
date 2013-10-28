34228ff02e1b9984d8581f39af5751769b1def3a
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-10-28 13:57:50 +0700

    metropolitan club - show name

--- app/controllers/metropolitan_clubs_controller.rb
+++ app/controllers/metropolitan_clubs_controller.rb
@@ -1,5 +1,6 @@
 class MetropolitanClubsController < ApplicationController
   def show
     @metropolitan_club = current_user.metropolitan_club
+    redirect_to edit_user_profile_path(current_user, current_user.profile), notice: "please update your current city" unless @metropolitan_club
   end
 end
