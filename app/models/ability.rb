36cb77654596e1da7b6c5a7d512c5cebcb52354e
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-10-31 16:17:10 +0700

    config cancan for update and metropolitan club

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,13 +3,15 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
+    club_ids = user.university.clubs.map(&:id)
     if user.has_role? :super_admin
       can :manage, :all
     elsif user.has_role? :university_admin
       can :manage, User, university_id: user.university_id
-      #can :manage, Club, university_id: user.university_id
-    #elsif user.has_role? :member
-    #  #can
+      can :manage, Update
+    else
+      can [:read, :create], Update, updateable_id: user.metropolitan_club.id, updateable_type: "Club"
+      can [:update, :destroy], Update, user_id: user.id
     end
   end
 end
