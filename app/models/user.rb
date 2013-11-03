21fa07fb8991241cbba5d2bac51f2d5e423e6651
Jim Ray <jimiray@gmail.com>
2013-11-03 16:58:29 -0700

    don't include super admin and university admin in search results

--- app/models/user.rb
+++ app/models/user.rb
@@ -107,15 +107,16 @@ class User < ActiveRecord::Base
     clubs.include?(club)
   end
 
-  class << self 
+  class << self
     def search_all(params)
       return where("1=1") if params.blank?
       search_name(params[:name]).search_location(params[:loc]).search_type(params[:type])
-      .search_major(params[:major]).search_graduation_year(params[:year]).search_professional_field(params[:field]).search_city(params[:city])
+      .search_major(params[:major]).search_graduation_year(params[:year]).search_professional_field(params[:field]).search_city(params[:city]).where("id != 1 AND id != 2")
     end
+
     def search_name(name)
       return where("1=1") if name.blank?
-      where("lower(first_name) like ? or lower(last_name) like ?", "%#{name.downcase}%", "%#{name.downcase}%")
+      where("lower(first_name) like ? or lower(last_name) like ?", "%#{name.downcase}%", "%#{name.downcase}%").where("id != 1 AND id != 2")
     end
 
     def search_city(city_name)
