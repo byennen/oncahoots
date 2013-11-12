406c885df4a1848335a011055594a030cc6d8837
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-11-12 14:39:10 +0700

    message display image,name as cahoots if user is super admin message display image,name as university if user is university admin

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,13 +7,7 @@ class ContactsController < ApplicationController
 
   def search
     contacts = current_user.relations.where("lower(first_name) like ? or lower(last_name) like ?", "%#{params[:term].downcase}%", "%#{params[:term].downcase}%")
-    results = []
-    contacts.each do |user|
-      results << {id: user.id, label: user.name, value: user.slug} if Relationship.reciprocal?(current_user, user)
-    end
-    respond_to do |format|
-      format.json {render json: results}
-    end
+    return_auto_json(contacts)
   end
 
   def multi_search
