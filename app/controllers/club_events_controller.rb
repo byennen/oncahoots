9dddd8ba81d560ee023b68de9da060a2087804ef
Bhanu Prasad <bhanuprasad143@gmail.com>
2013-11-13 11:38:56 +0700

    Club calendar  show event by slide  create/edit event  super-admin/university admin can manage private area

--- app/controllers/club_events_controller.rb
+++ app/controllers/club_events_controller.rb
@@ -5,22 +5,12 @@ class ClubEventsController < ApplicationController
   def create
     @event = @club.events.new(params[:event])
     @event.university_id = @club.university_id
-    if @event.save
-      redirect_to university_club_path(@university, @club)
-    else
-      club_init
-      render :template => university_club_path(@university, @club)
-    end
+    @event.save
   end
 
   def update
     @event = @club.events.find(params[:id])
-    if @club.update_attributes(params[:club])
-      redirect_to university_club_path(@university, @club), alert: "access denied."
-    else
-      club_init
-      render :template => university_club_path(@university, @club)
-    end
+    @event.update_attributes(params[:event])
   end
 
   def destroy
@@ -36,22 +26,10 @@ class ClubEventsController < ApplicationController
   private
     def ensure_club
       @club = Club.find params[:club_id]
-      if current_user.manage_club?(@club)
+      if current_user.member_of?(@club) || current_user.manage_club?(@club)
         true
       else
         redirect_to university_club_path(@university, @club), alert: "access denied."
       end
-    end
-
-    def club_init
-      @university = @club.university
-      @membership = Membership.new
-      @members = @club.users
-      @memberships = @club.memberships
-      @current_membership = @club.memberships.find_by_user_id(current_user.id)
-      @admins = @club.memberships.where(admin: true)
-      @conversations = current_user.manage_club?(@club) ? @club.mailbox.inbox : current_user.conversations_for(@club)
-      @invitation = Invitation.new
-      @event ||= Event.new
     end
 end
