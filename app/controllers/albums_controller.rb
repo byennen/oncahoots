c16e886e5398fa5b2b6ed6d67dd2099b2bbcbd06
Bhanu Prasad <bhanuprasad143@gmail.com>
2014-01-20 16:19:48 +0700

    upload image for album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -8,6 +8,13 @@ class AlbumsController < ApplicationController
     respond_to :js
   end
 
+  def update
+    @album = Album.find params[:id]
+    @album.update_attributes(params[:album])
+    @album.save
+    redirect_to university_club_path(@club.university, @club)
+  end
+
   private
     def load_resource
       @club = Club.find params[:club_id]
