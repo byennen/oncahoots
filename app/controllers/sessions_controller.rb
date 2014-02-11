4339c39d42653de5605ed9958a7759892f07c6f3
Andy Maleh <andy.am@gmail.com>
2014-02-11 13:02:12 -0500

    Fixed authentication. Portfolio fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,4 @@
 class SessionsController < Devise::SessionsController
   layout 'sessions'
   skip_before_filter :check_completed_info
-
-  def create
-    auth_options = { :recall => 'home#index', :scope => :user }
-    resource = warden.authenticate!(auth_options)
-  end
 end
