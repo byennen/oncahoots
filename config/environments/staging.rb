226093e1ef711b39ccc4d78799a2657bc68c16fd
Lance Ennen <byennen@gmail.com>
2014-02-19 16:21:16 -0600

    set staging.rb to precompile assests :smiley_cat:

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -15,7 +15,11 @@ CahootsConnect::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # Compress JavaScripts and CSS
+  config.assets.js_compressor  = :uglifier
+  config.assets.css_compressor = :sass
 
   # Generate digests for assets URLs
   config.assets.digest = true
