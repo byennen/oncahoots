eac33422e3967a2d2bd7a23914207401b79b3288
Lance Ennen <byennen@gmail.com>
2014-02-19 16:39:25 -0600

    update staging for compiled assets :smile:

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,7 +19,6 @@ CahootsConnect::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :sass
 
   # Generate digests for assets URLs
   config.assets.digest = true
